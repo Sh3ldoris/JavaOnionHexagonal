1aa766923069efb1431c3699176a07274b013bf2
Adam Lany <adam.lany@posam.sk>
2026-05-03 10:49:44 +0200

    feat(work-order): WO Completed event listener

---
+++ water-application/src/main/java/com/waterconnect/application/eventlistener/WorkOrderCompletedEventListener.java
@@ -0,0 +1,47 @@
+package com.waterconnect.application.eventlistener;
+
+import java.util.Objects;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.waterconnect.application.usecase.ActivateServicePointConnectionUseCase;
+import com.waterconnect.domain.event.WorkOrderCompletedEvent;
+import com.waterconnect.domain.exception.BusinessRuleViolationException;
+import com.waterconnect.domain.exception.EntityNotFoundException;
+import com.waterconnect.domain.port.outbound.ServicePointRepository;
+import com.waterconnect.domain.port.outbound.WorkOrderRepository;
+
+@Component
+public class WorkOrderCompletedEventListener {
+
+    private final ActivateServicePointConnectionUseCase activateServicePointConnectionUseCase;
+    private final WorkOrderRepository workOrderRepository;
+
+    public WorkOrderCompletedEventListener(
+            ActivateServicePointConnectionUseCase activateServicePointConnectionUseCase,
+            WorkOrderRepository workOrderRepository,
+            ServicePointRepository servicePointRepository
+    ) {
+        this.activateServicePointConnectionUseCase = activateServicePointConnectionUseCase;
+        this.workOrderRepository = workOrderRepository;
+    }
+
+    @EventListener
+    public void onUserCreated(WorkOrderCompletedEvent event) {
+        // Get wo
+        var workOrder = this.workOrderRepository
+                .findById(event.workOrderId())
+                .orElse(null);
+
+        if (Objects.isNull(workOrder)) {
+            throw new EntityNotFoundException("WorkOrder",  event.workOrderId());
+        }
+
+        if (Objects.isNull(workOrder.getServicePointId())) {
+            throw new BusinessRuleViolationException("ServicePoint ID is not assigned to WorkOrder: " + workOrder.getWorkOrderId());
+        }
+
+        this.activateServicePointConnectionUseCase.execute(workOrder.getServicePointId());
+    }
+}
