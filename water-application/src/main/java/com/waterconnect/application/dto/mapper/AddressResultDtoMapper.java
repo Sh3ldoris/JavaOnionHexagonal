118f470426bc9c5ac311a64db732bfefbab3b6d4
Adam Lany <adam.lany@posam.sk>
2026-05-11 21:57:20 +0200

    test(application-mapper): Added null checks

--- water-application/src/main/java/com/waterconnect/application/dto/mapper/AddressResultDtoMapper.java
+++ water-application/src/main/java/com/waterconnect/application/dto/mapper/AddressResultDtoMapper.java
@@ -7,6 +7,10 @@ public class AddressResultDtoMapper {
     private AddressResultDtoMapper() {}
 
     public static AddressResultDto fromDomain(Address address) {
+        if (address == null) {
+            return null;
+        }
+
         return new AddressResultDto(address.street(), address.city(), address.postalCode(), address.country());
     }
 }
--- water-application/src/main/java/com/waterconnect/application/dto/mapper/CustomerResultDtoMapper.java
+++ water-application/src/main/java/com/waterconnect/application/dto/mapper/CustomerResultDtoMapper.java
@@ -1,5 +1,7 @@
 package com.waterconnect.application.dto.mapper;
 
+import java.util.Objects;
+
 import com.waterconnect.application.dto.CustomerResultDto;
 import com.waterconnect.application.dto.enums.CustomerType;
 import com.waterconnect.domain.model.aggregate.Customer;
@@ -8,13 +10,25 @@ public class CustomerResultDtoMapper {
     private CustomerResultDtoMapper() {}
 
     public static CustomerResultDto fromDomain(Customer customer) {
+        if (Objects.isNull(customer)) {
+            return null;
+        }
+
         var contactInfo = customer.getContactInfo();
-        var address = contactInfo.address();
+        var isContactInfo = Objects.nonNull(contactInfo);
 
-        var addressResult = AddressResultDtoMapper.fromDomain(address);
+        var addressResult = AddressResultDtoMapper.fromDomain(
+                isContactInfo ? contactInfo.address() : null
+        );
 
-        return new CustomerResultDto(customer.getCustomerId(), customer.getFullName(),
-                CustomerType.fromString(customer.getCustomerType().name()), contactInfo.email(),
-                contactInfo.phone(), addressResult, customer.getRegisteredAt());
+        return new CustomerResultDto(
+                customer.getCustomerId(),
+                customer.getFullName(),
+                CustomerType.fromString(customer.getCustomerType().name()),
+                isContactInfo ? contactInfo.email() : null,
+                isContactInfo ? contactInfo.phone() : null,
+                addressResult,
+                customer.getRegisteredAt()
+        );
     }
 }
